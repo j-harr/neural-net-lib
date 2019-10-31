df2d6f77b99f180013dc128d30d9fd57df244c8e
Jacob Harrison <jacob-harrison@outlook.com>
2019-10-30 22:23:55 -0500

    Add some initial unit tests and basic constructors for NeuralLayer.

--- NeuNetTests/UnitTest1.cs
+++ NeuNetTests/UnitTest1.cs
@@ -1,15 +1,32 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NeuNets;
+using System;
 
 namespace NeuNetTests
 {
     [TestClass]
-    public class UnitTest1
+    public class NeuralLayerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DefaultConstructorHasSize0()
         {
-            Assert.AreEqual(4, 3 + 1);
+            NeuralLayer neuralLayer1 = new NeuralLayer();
+            Assert.AreEqual(0, neuralLayer1.getSize());
+        }
+
+        [TestMethod]
+        public void ParametrizedConstructorWithRangeChecks()
+        {
+            NeuralLayer nl = new NeuralLayer(1);
+            Assert.AreEqual(1, nl.getSize());
+
+            nl = new NeuralLayer(12);
+            Assert.AreEqual(12, nl.getSize());
+
+            nl = new NeuralLayer(0);
+            Assert.AreEqual(0, nl.getSize());
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => nl = new NeuralLayer(-3));
         }
     }
 }
---
+++ NeuNets/NeuralLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuNets
+{
+    public class NeuralLayer
+    {
+        private int size;
+        public NeuralLayer()
+        {
+            this.size = 0;
+        }
+
+        public NeuralLayer(int size)
+        {
+            if (size < 0) throw new ArgumentOutOfRangeException();
+            this.size = size;
+        }
+
+        public int getSize() { return this.size; }
+    }
+}
