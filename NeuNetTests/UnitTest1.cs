d1a5e4315453d8c9953c08afba9862e93ed659e1
Jacob Harrison <jacob-harrison@outlook.com>
2019-10-30 21:52:33 -0500

    Add unstaged items. Oops :o

---
+++ NeuNetTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuNets;
+
+namespace NeuNetTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4, 3 + 1);
+        }
+    }
+}
